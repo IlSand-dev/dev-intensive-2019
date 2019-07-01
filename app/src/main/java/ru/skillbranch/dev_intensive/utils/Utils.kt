7277acedb5d57f6b170fd799b6f02018976d92a8
Ilya_Pesotskiy <ilya@d3en.ru>
2019-07-01 20:48:03 +0600

    parceFullName fixed and toInitials realized

--- app/src/main/java/ru/skillbranch/dev_intensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/dev_intensive/utils/Utils.kt
@@ -1,21 +1,22 @@
 package ru.skillbranch.dev_intensive.utils
 
 object Utils {
-    fun parseFullName(fullNameString: String?): Pair<String?, String?> {
-        //TODO FIX ME!!!
-        val parts: List<String>? = fullNameString?.split(" ")
-
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
-
-        return firstName to lastName
+    fun parseFullName(fullNameString: String?): Pair<String?, String?> = when (fullNameString.isNullOrBlank()) {
+        true -> null to null
+        else -> {
+            val parts: List<String> = fullNameString.split(" ")
+            if (parts.size == 1) parts[0] to null else parts[0] to parts[1]
+        }
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun toInitials(firstName: String?, lastName: String?) = when (firstName.isNullOrBlank() to lastName.isNullOrBlank()) {
+        true to true -> null
+        false to true -> "${firstName!![0].toUpperCase()}"
+        true to false -> "${lastName!![0].toUpperCase()}"
+        else -> "${firstName!![0].toUpperCase()}${lastName!![0].toUpperCase()}"
     }
 }
